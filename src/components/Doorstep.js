9863c86d96801c5c08ee8013800d9a5c03756e24
basithahamed <basithhahamed@gmail.com>
2023-08-25 16:17:51 +0530

    Test Deploy

---
+++ src/components/Doorstep.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components"
+function Doorstep() {
+  return (
+    <Container>
+        <Leftitem>
+            
+        </Leftitem>
+        <Rightitem>
+
+        </Rightitem>
+    </Container>
+  )
+}
+const Leftitem=styled.div``
+const Rightitem=styled.div``
+export default Doorstep
